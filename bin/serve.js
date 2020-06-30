f1b7023455bfc0ab4453af07632841064eafcfbd
Jérémie Pichon <jeremie.pichon@genesys.com>
2020-06-30 13:29:24 +0200

    fix unit tests

--- bin/serve.js
+++ bin/serve.js
@@ -1,12 +1,12 @@
 const c = require('ansi-colors');
 const express = require('express');
 
-module.exports.serve = async (static) => {
+module.exports.serve = async (staticDir) => {
   const app = express();
 
   const port = 5000;
 
-  app.use('/', express.static(static));
+  app.use('/', express.static(staticDir));
 
   app.listen(port, function () {
     console.log(c.green(`Documentation is being served here: ${c.cyan(`http://localhost:${port}`)}`));
